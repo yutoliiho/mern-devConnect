2b6a7474456ec7a277c904e952a9fe7988ca2456
Tong Yu <yutongsf@gmail.com>
2020-04-10 15:41:19 -0700

    initial commit -2

---
+++ app.js
@@ -0,0 +1,19 @@
+const log = console.log;
+
+function bla() {
+  if (1 + 1 !== 3) {
+    return false;
+  }
+  return true;
+}
+
+bla();
+function blaa() {
+  if (!bla) {
+  }
+}
+
+// mongoose: a layar sit on top of the db so we could interact with it.
+
+// 3:42 (security)
+// add new user:
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const log = console.log;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello world, API is running');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  log(`server is up on port ${PORT}`);
+});
